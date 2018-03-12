a67b42f8320cf57ac0a7ef06b02a9f32bdb27407
AnirudhGurjale <37309680+AnirudhGurjale@users.noreply.github.com>
2018-03-12 17:30:33 -0400

    Update primes.py

--- primes.py
+++ primes.py
@@ -9,3 +9,24 @@ def isprime(p):
       return False
     if (i == k + 1):
       return True
+
+def nextelement(S):
+  l = S[-1]
+  for k in range(l + 1, 2*l):
+    m = int(k**(1/2))
+    for i in S:
+      if (i > m):
+        return k
+      if (k%i == 0):
+        break
+        
+def iterate(S):
+  return S + [nextelement(S)]
+
+def primesupto(N):
+  S = [2]
+  if (N <= 1):
+    return []
+  while (S[-1] < N):
+    S = iterate(S)
+  return S[:-1]
