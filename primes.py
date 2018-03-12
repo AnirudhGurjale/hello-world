fa476079f815cf3d2aa8e65286260c5549c5557d
AnirudhGurjale <37309680+AnirudhGurjale@users.noreply.github.com>
2018-03-12 17:15:10 -0400

    Update primes.py

--- primes.py
+++ primes.py
@@ -1,2 +1,11 @@
 def isprime(p):
-  if ()
+  if (p <= 1):
+    return False
+  if (p == 2)|(p == 3):
+    return True
+  k = int(p**(1/2))
+  for i in range(2, k + 2):
+    if (p%i == 0):
+      return False
+    if (i == k + 1):
+      return True
